18ff3cfe91cc388a0d0651264f839fbda17ee599
Allyson Tom <goforth@Allysons-MBP-5.app.byu.edu>
2017-01-10 12:48:53 -0700

    day 1 newton's method

--- sol.py
+++ sol.py
@@ -1,6 +1,6 @@
 def Newtons_Method(func, func_prime, x_0, iters=100, tol=1e-5):
   """A direct implementation of Newton's Method
-     (For sanity assume that func and func_prime define there own division correctly,
+     (For sanity assume that func and func_prime define their own division correctly,
      that is, don't cast anything to a float)
      params: func: a function,
              fuc_prime: its direvative
@@ -10,4 +10,19 @@ def Newtons_Method(func, func_prime, x_0, iters=100, tol=1e-5):
      returns: a root(int) if found
               None(none-type) else
   """
-  pass
+  i = 1
+  x1 = x_0 - func(x_0)/func_prime(x_0)
+  while (i < iters):
+    if (abs(x1 - x_0) > tol):
+      i += 1
+      x_0 = x1
+      x1 = x_0 - func(x1)/func_prime(x1)
+    else:
+      return x1
+"""
+def func(x):
+  return float(x**2) - 5.
+def func_prime(x):
+  return 2.*x
+x_0 = -5.0
+print Newtons_Method(func, func_prime, x_0)"""
